fcda008f1f718d2fc88e7a3dd31a0c736825753a
Lucas Lopes <73676095+LucasLopes285@users.noreply.github.com>
2021-05-04 11:09:56 -0300

    Primeira classe: Posicao (#2)

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,10 +1,13 @@
 package aplicacao;
 
+import camadatabuleiro.Posicao;
+
 public class Programa {
 
 	public static void main(String[] args) {
      
-		System.out.println("Ola mundo");
+		Posicao pos = new Posicao(3, 5);
+		System.out.println(pos);
 	}
 
 }
