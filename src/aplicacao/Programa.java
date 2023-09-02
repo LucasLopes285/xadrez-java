00ce97cd6ee887112e90e5deee55afc1a469db02
Lucas Lopes <lucas.haddad15@gmail>
2023-09-02 20:04:20 -0300

    Primeira classe: Posicao

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,11 +1,12 @@
 package aplicacao;
 
+import jogotabuleiro.Posicao;
+
 public class Programa {
 
 	public static void main(String[] args) {
-		
-		System.out.println("Ola");
-    
-	}
+		Posicao pos = new Posicao(3, 5);
+		System.out.println(pos);
+		}
 
 }
